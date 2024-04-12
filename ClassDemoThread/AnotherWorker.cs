03677ea915a2a641e96b707047bc4257cf270b82
Peter Levinsky <pele@zealand.dk>
2024-04-12 13:27:20 +0200

    Med semaforer

---
+++ ClassDemoThread/AnotherWorker.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoThread
+{
+    public class AnotherWorker
+    {
+        private Random _rnd = new Random(DateTime.Now.Microsecond);
+
+        private Semaphore sem = new Semaphore(10,20);
+
+        public void Start()
+        {
+            Task t1 = Task.Run(() => Sender());
+            Task t2 = Task.Run(() => Modtager());
+
+            t1.Wait();
+            t2.Wait();
+        }
+
+
+
+        public void Sender()
+        {
+            for (int i = 0; i < 20; i++) 
+            {
+                sem.WaitOne();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write($"Item {i} ");
+                Thread.Sleep(_rnd.Next(30));
+                Console.WriteLine("er sendt");
+            }
+        }
+
+        public void Modtager()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(_rnd.Next(30));
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write($"Item {i} er modtaget");
+                sem.Release();
+            }
+            Console.ReadLine();
+
+            for (int i = 5; i < 20; i++)
+            {
+                Thread.Sleep(_rnd.Next(30));
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($"Item {i} er modtaget");
+                sem.Release();
+            }
+        }
+    }
+}
--- ClassDemoThread/Program.cs
+++ ClassDemoThread/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ClassDemoThread;
 using ClassDemoThreadTask;
 
 ThreadWorker worker = new ThreadWorker();
@@ -7,11 +8,18 @@ ThreadWorker worker = new ThreadWorker();
 //worker.TestDelegate();
 
 
-worker.ExampleThread();
+//worker.ExampleThread();
 
 //worker.StartTaskTest();
 
 //Thread.Sleep(1000);
+
+AnotherWorker w2 = new AnotherWorker();
+w2.Start();
+
+
+
+Console.ReadLine();
 
 
 
